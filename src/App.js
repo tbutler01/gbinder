7e18ba794d743300dab3ecdd8efc7d3fd3ee3496
Taurey Butler <taureybutler@gmail.com>
2021-05-21 02:28:33 -0400

    79. update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Switch, Route, Link, withRouter } from 'react-router-dom';
+
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const Page = ({ title }) => (
+  <div>
+    <h1>{title.toUpperCase()} PAGE</h1>
+    <Link to='/'>Go HOME</Link>
+  </div>
+);
+
+
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/songs/ballad' component={() => <Page title='ballad' />} />
+        <Route path='/songs/swing' component={() => <Page title='swing' />} />
+        <Route path='/songs/latin' component={() => <Page title='latin' />} />
+        <Route path='/songs/blues' component={() => <Page title='blues' />} />
+        <Route path='/songs/funk' component={() => <Page title='funk' />} />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
                 {
                     id: 0,
                     title: 'ballad',
-                    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61Z-qRw050L._AC_SX522_.jpg'
+                    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61Z-qRw050L._AC_SX522_.jpg',
+                    linkUrl: 'songs/ballad'
                 },
                 {
                     id: 1,
                     title: 'swing',
-                    imageUrl: 'https://cdn2.jazztimes.com/2011/12/OscarPeterson_HPF.jpg'
+                    imageUrl: 'https://cdn2.jazztimes.com/2011/12/OscarPeterson_HPF.jpg',
+                    linkUrl: 'songs/swing'
                 },
                 {
                     id: 2,
                     title: 'latin',
-                    imageUrl: 'https://vmp-www.imgix.net/images/album_9.original.jpg?auto=format&w=1660&h=1660'
+                    imageUrl: 'https://vmp-www.imgix.net/images/album_9.original.jpg?auto=format&w=1660&h=1660',
+                    linkUrl: 'songs/latin'
                 },
                 {
                     id: 3,
                     title: 'blues',
                     size: 'large',
-                    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51dEb2ab%2BAL.__AC_SX300_SY300_QL70_ML2_.jpg'
+                    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51dEb2ab%2BAL.__AC_SX300_SY300_QL70_ML2_.jpg',
+                    linkUrl: 'songs/blues'
                 },
                 {
                     id: 4,
                     title: 'funk',
                     size: 'large',
-                    imageUrl: 'https://media.pitchfork.com/photos/5e84ffabc2ed2400096ee289/2:1/w_1000/Head%20Hunters_Herbie%20Hancock.jpg'
+                    imageUrl: 'https://media.pitchfork.com/photos/5e84ffabc2ed2400096ee289/2:1/w_1000/Head%20Hunters_Herbie%20Hancock.jpg',
+                    linkUrl: 'songs/funk'
                 },
             ]
         };
@@ -45,8 +50,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ id, title, imageUrl, size }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
